60d4e245c92adca398d060edfa810326c304a0e2
shreemarella <mvsrikanth96@gmail.com>
2020-03-07 17:54:01 -0500

    [SM] created customer swift file to enter the details and datatypes

---
+++ C0771612_MidTerm_MAD3115W2020/Customer.swift
@@ -0,0 +1,14 @@
+//
+//  Customer.swift
+//  C0771612_MidTerm_MAD3115W2020
+//
+//  Created by Shree Marella on 2020-03-07.
+//  Copyright © 2020 Shree Marella. All rights reserved.
+//
+
+import Foundation
+
+class Customer
+{
+    
+}
