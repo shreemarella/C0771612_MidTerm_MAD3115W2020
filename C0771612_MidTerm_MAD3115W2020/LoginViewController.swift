70e617eb40a69a292ad43becdbc71894e1755b06
shreemarella <mvsrikanth96@gmail.com>
2020-03-07 18:43:54 -0500

    [SM] created func for displaying errors in email and password

--- C0771612_MidTerm_MAD3115W2020/LoginViewController.swift
+++ C0771612_MidTerm_MAD3115W2020/LoginViewController.swift
@@ -17,11 +17,68 @@ class LoginViewController: UIViewController {
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        RememberMe()
         
+    }
+    
+    private func RememberMe()
+    {
+        let userDefault = UserDefaults.standard
+        
+        if let userName = userDefault.string(forKey: "Email")
+        {
+            txtfdEmail.text = userName
+            
+            if let passWord = userDefault.string(forKey: "Password")
+            {
+                txtfdPassword.text = passWord
+            }
+        }
         
     }
 
-    @IBAction func btnLogin(_ sender: UIButton) {
+    @IBAction func btnLogin(_ sender: UIButton)
+    {
+        if self.txtfdEmail.text == "Shree.Marella@gmail.com" && self.txtfdPassword.text == "Shree696"
+        {
+            let userDefault = UserDefaults.standard
+            if sthRememberMe.isOn
+            {
+                userDefault.setValue(txtfdEmail.text, forKey: "Email")
+                userDefault.set(txtfdPassword.text, forKey: "Password")
+            }
+            else
+            {
+                userDefault.removeObject(forKey: "Email")
+                userDefault.removeObject(forKey: "Password")
+            }
+        }
+        else
+        {
+            if self.txtfdEmail.text == " "
+            {
+                alertMessage(message: "Empty Field")
+            }
+            else if self.txtfdPassword.text == " "
+            {
+                alertMessage(message: "Empty Field")
+            }
+            else
+            {
+                alertMessage(message: "Enter Vaild Email ID or Password")
+                
+            }
+        }
+    }
+    
+    func alertMessage(message : String)
+    {
+        
+        let alertController = UIAlertController(title: "Wrong Input", message: "Check Your Email Or Password", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Invalid Email Or Password", style: .default))
+
+        self.present(alertController, animated: true, completion: nil)
+        
     }
     
 }
