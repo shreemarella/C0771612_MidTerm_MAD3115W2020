f5412bc7ca2bb1bde518b52214f0bf4032d4d14f
shreemarella <mvsrikanth96@gmail.com>
2020-03-07 16:00:44 -0500

    Initial Commit

---
+++ C0771612_MidTerm_MAD3115W2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C0771612_MidTerm_MAD3115W2020
+//
+//  Created by Shree Marella on 2020-03-07.
+//  Copyright © 2020 Shree Marella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
