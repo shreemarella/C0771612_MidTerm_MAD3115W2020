18b90584c707e8723988d84b7cbb78cb63740690
shreemarella <mvsrikanth96@gmail.com>
2020-03-13 18:30:24 -0400

    [SM] created func for segment for displaying their own textfields of their types

--- C0771612_MidTerm_MAD3115W2020/AddBillViewController.swift
+++ C0771612_MidTerm_MAD3115W2020/AddBillViewController.swift
@@ -11,6 +11,16 @@ import UIKit
 class AddBillViewController: UIViewController
 {
     var customer : Customer?
+    @IBOutlet weak var txtfBillId: UITextField!
+    @IBOutlet weak var txtfBillDate: UITextField!
+    @IBOutlet weak var txtfMobileManuName: UITextField!
+    @IBOutlet weak var txtfPlanName: UITextField!
+    @IBOutlet weak var txtfIntGbUsedInternet: UITextField!
+    @IBOutlet weak var txtfMinUsed: UITextField!
+    @IBOutlet weak var txtfProviderName: UITextField!
+    @IBOutlet weak var txtfMobilenetGbUsed: UITextField!
+    @IBOutlet weak var txtfAgencyName: UITextField!
+    @IBOutlet weak var txtfUnitsConsumed: UITextField!
     
     override func viewDidLoad()
     {
@@ -19,4 +29,43 @@ class AddBillViewController: UIViewController
         self.title = "Add New Bill Record"
     }
 
+    @IBAction func segmentBillType(_ sender: UISegmentedControl)
+    {
+        if sender.selectedSegmentIndex == 0
+        {
+            
+            txtfMobileManuName.isHidden = true
+            txtfPlanName.isHidden = true
+            txtfIntGbUsedInternet.isHidden = true
+            txtfMinUsed.isHidden = true
+            txtfProviderName.isHidden = true
+            txtfMobilenetGbUsed.isHidden = true
+        }
+        else
+        if sender.selectedSegmentIndex == 1
+        {
+            txtfIntGbUsedInternet.isHidden = true
+            txtfProviderName.isHidden = true
+            txtfAgencyName.isHidden = true
+            txtfUnitsConsumed.isHidden = true
+            
+        }
+        else
+        if sender.selectedSegmentIndex == 2
+        {
+            txtfMobileManuName.isHidden = true
+            txtfPlanName.isHidden = true
+            txtfMinUsed.isHidden = true
+            txtfMobilenetGbUsed.isHidden = true
+            txtfAgencyName.isHidden = true
+            txtfUnitsConsumed.isHidden  = true
+        }
+    }
+    
+    
+    @IBAction func btnAddBillRecord(_ sender: UIButton)
+    {
+        
+        
+    }
 }
