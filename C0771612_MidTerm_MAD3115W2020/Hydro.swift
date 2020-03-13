ad59a55e9920e2896e671dd6a7047470c83015e8
shreemarella <mvsrikanth96@gmail.com>
2020-03-13 00:30:12 -0400

    [SM] created hydro class and variables and initialized them

---
+++ C0771612_MidTerm_MAD3115W2020/Hydro.swift
@@ -0,0 +1,26 @@
+//
+//  Hydro.swift
+//  C0771612_MidTerm_MAD3115W2020
+//
+//  Created by Shree Marella on 2020-03-13.
+//  Copyright © 2020 Shree Marella. All rights reserved.
+//
+
+import Foundation
+
+class Hydro : Bills
+{
+    var agencyName : String
+    var unitsConsumed : Double
+    var ratePerUnit : Double = 15.5
+    
+     init(billID: String, billDate: Date, billType: BillType, agencyName: String,unitsConsumed: Double)
+    {
+        self.agencyName = agencyName
+        self.unitsConsumed = unitsConsumed
+        super.init(billID: billID, billDate: billDate, billType: billType)
+        self.billAmount = caluclateTotalBill()
+    }
+    
+    
+}
