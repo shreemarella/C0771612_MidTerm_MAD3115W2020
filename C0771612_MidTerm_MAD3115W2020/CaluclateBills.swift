b0ea3ca313a2b3de8dd1a56c40cd11c08f207f20
shreemarella <mvsrikanth96@gmail.com>
2020-03-12 23:18:10 -0400

    [SM] created protocol for calculating total bill

---
+++ C0771612_MidTerm_MAD3115W2020/CaluclateBills.swift
@@ -0,0 +1,15 @@
+//
+//  CaluclateBill.swift
+//  C0771612_MidTerm_MAD3115W2020
+//
+//  Created by Shree Marella on 2020-03-12.
+//  Copyright © 2020 Shree Marella. All rights reserved.
+//
+
+import Foundation
+
+
+protocol CaluclateBills
+{
+    func calculateTotalBill() -> Double
+}
