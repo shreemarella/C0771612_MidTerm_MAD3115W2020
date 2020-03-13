c9d379dca1bccd5a294a99a4d35851ff5e6baa8b
shreemarella <mvsrikanth96@gmail.com>
2020-03-13 02:19:37 -0400

    [SM] added data of mobile in singleton class

--- C0771612_MidTerm_MAD3115W2020/DataRepoSingleton.swift
+++ C0771612_MidTerm_MAD3115W2020/DataRepoSingleton.swift
@@ -33,11 +33,16 @@ class DataRepoSingleton
     
     func loadData()
     {
-        custList.updateValue(Customer(customerId: "C001", firstName: "Shree", lastName: "Marella", emailId: "Shree.Marella@gmail.com"),forKey: "C001")
-        custList.updateValue(Customer(customerId: "C002", firstName: "Shreya", lastName: "vaghasiya", emailId: "Shreyavaghasiya@gmail.com"),forKey: "C002")
-        custList.updateValue(Customer(customerId: "C003", firstName: "Kasyap", lastName: "jhaveri", emailId: "kasyap@gmail.com"),forKey: "C003")
-        custList.updateValue(Customer(customerId: "C004", firstName: "Charmi", lastName: "Patel", emailId: "Charmi@gmail.com"),forKey: "C004")
-        custList.updateValue(Customer(customerId: "C005", firstName: "Avani", lastName: "Patel", emailId: "Avani@gmail.com"),forKey: "C005")
+        var c1=Customer(customerId: "C001", firstName: "Shree", lastName: "Marella", emailId: "Shree.Marella@gmail.com")
+        var c2=Customer(customerId: "C002", firstName: "Shreya", lastName: "vaghasiya", emailId: "Shreyavaghasiya@gmail.com")
+        var c3=Customer(customerId: "C003", firstName: "Kasyap", lastName: "jhaveri", emailId: "kasyap@gmail.com")
+        var c4=Customer(customerId: "C004", firstName: "Charmi", lastName: "Patel", emailId: "Charmi@gmail.com")
+        var c5=Customer(customerId: "C005", firstName: "Avani", lastName: "Patel", emailId: "Avani@gmail.com")
+        
+        var m1 = Mobile(billID: "MOB001", billDate: Date.from(year: 2019, month: 10, day: 24) ?? Date(), billType: BillType.MOBILE, mobileManufacturerName: "Galaxy Samsung", planName: "Prepaid Talk + Text plan", mobileNumber: "+1(567)-305-8907", internetGbUsed: 9.0, minutesUsed: 498)
+        var m2 = Mobile(billID: "MOB002", billDate: Date.from(year: 2019, month: 06, day: 06) ?? Date(), billType: BillType.MOBILE, mobileManufacturerName: "Apple", planName: "Prepaid + Text plan", mobileNumber: "8199210696", internetGbUsed: 7.5, minutesUsed: 420)
+
+        
     }
     
 }
