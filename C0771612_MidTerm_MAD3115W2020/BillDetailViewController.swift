e26301c79c5391cf1a49b18c3d2c204b405e95f2
shreemarella <mvsrikanth96@gmail.com>
2020-03-12 21:10:51 -0400

    [SM] created new taco class of details bill VC

---
+++ C0771612_MidTerm_MAD3115W2020/BillDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BillDetailViewController.swift
+//  C0771612_MidTerm_MAD3115W2020
+//
+//  Created by Shree Marella on 2020-03-12.
+//  Copyright © 2020 Shree Marella. All rights reserved.
+//
+
+import UIKit
+
+class BillDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
